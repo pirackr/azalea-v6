2cbca0b7e5a6c158d8540c0007c10bec715d643d
Pirackr <pirackr.inbox@gmail.com>
2026-02-01 07:45:04 +0000

    feat: remove nix and just install stuff normally

--- kubernetes/apps/azalea-dev/image/entrypoint.sh
+++ kubernetes/apps/azalea-dev/image/entrypoint.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 mkdir -p /var/run/sshd
 
-if [[ ! -x /home/pirackr/.nix-profile/bin/nix ]]; then
-  su - pirackr -c "curl -L https://nixos.org/nix/install -o /tmp/nix-install.sh"
-  su - pirackr -c "sh /tmp/nix-install.sh --no-daemon"
-  rm -f /tmp/nix-install.sh
-fi
-
 if [[ ! -f /etc/ssh/ssh_host_ed25519_key ]]; then
   ssh-keygen -A
 fi
